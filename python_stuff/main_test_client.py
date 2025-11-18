5f941b421c56f9d3c857e355ef48610b42538814
LavaTigerUnicrn <nolanlance711@gmail.com>
2025-11-17 19:44:27 -0800

    Made tweaks to the popups for better customizability and smoother looks

--- python_stuff/main_test_client.py
+++ python_stuff/main_test_client.py
@@ -13,7 +13,7 @@ if __name__ == "__main__":
     easy_client.add_button([0,0,50,50],"Button Numero Uno")
     easy_client.add_button([500,500,100,50],"Button 2")
     easy_client.update_window([50,50,1000,1000])
-    easy_client.add_popup("Hello!")
+    easy_client.add_popup("Hello!",(0,0,0),(255,255,255),(250,300))
     
     while True:
         time.sleep(0.1)
--- python_stuff/python_acc_lib/settings.py
+++ python_stuff/python_acc_lib/settings.py
@@ -13,6 +13,7 @@ POPUP_WINDOW_PADDING = 10 # Padding from top-left of screen for popups (px)
 POPUP_TEXT_PADDING = 10 # Padding for the text within the padding (px)
 POPUP_PADDING = 5 # Padding for stacked popups next to each other (px)
 POPUP_MAX_TIME = 5 # Max time for popups to exist
+POPUP_DEFAULT_COLOR = (228,83,76) # Default color for popups to appear as
 
 # TTS
 TTS_VOICE = "en-US-EmmaMultilingualNeural"
