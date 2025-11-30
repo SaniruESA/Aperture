d1cf721e6b62bf5447cfe7bfdd150918057eabdf
LavaTigerUnicrn <nolanlance711@gmail.com>
2025-11-29 19:04:54 -0800

    Made working build.py

---
+++ python_stuff/python_acc_lib/__main__.py
@@ -0,0 +1,14 @@
+"""
+Quickly runs the server
+"""
+
+from . import server
+from . import logger
+from .logger.basic_logs import *
+
+if __name__ == "__main__":
+    logger.set_stdout()
+    logger.clear()
+    print("\x1b[2J\x1b[HServer Starting...",end="")
+
+    server.fast_start()
