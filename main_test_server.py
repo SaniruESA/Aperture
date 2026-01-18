0aad208918122d1fb16bab92d4a03c3e36beb4bb
D3BaNaNa <videep.mannava@gmail.com>
2026-01-17 21:46:40 -0800

    One more fix

--- main_test_server.py
+++ main_test_server.py
@@ -1,3 +1,10 @@
+from aperture_library import verify_lib
+
+# For testing purposes
+CHECK_LIBS = True
+if CHECK_LIBS:
+    verify_lib()
+
 import aperture_library.server as server
 import aperture_library.logger as logger
 from aperture_library.logger.basic_logs import *
